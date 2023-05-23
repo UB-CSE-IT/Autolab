df7eafc042d3e3ed822acf05a5794a2bc48896b0
Nicholas Myers <32116122+NicholasMy@users.noreply.github.com>
2023-05-23 15:23:55 -0400

    Update auth callbacks for Shibboleth

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,29 +1,4 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
-  def facebook
-    if user_signed_in?
-      if data = request.env["omniauth.auth"]
-        # add this authentication object to current user
-        if current_user.authentications.where(provider: data["provider"],
-                                              uid: data["uid"]).empty?
-          current_user.authentications.create(provider: data["provider"],
-                                              uid: data["uid"])
-        end
-      end
-      redirect_to(root_path) && return
-    else
-      @user = User.find_for_facebook_oauth(request.env["omniauth.auth"], current_user)
-
-      if @user
-        sign_in_and_redirect @user, event: :authentication # this will throw if @user is not activated
-        set_flash_message(:notice, :success, kind: "Facebook") if is_navigational_format?
-        return
-      else
-        # automatic cleanup of devise.* after sign in
-        session["devise.facebook_data"] = request.env["omniauth.auth"].except("extra")
-        redirect_to(new_user_registration_url) && return
-      end
-    end
-  end
 
   def google_oauth2
     if user_signed_in?
@@ -63,19 +38,32 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
       @user = User.find_for_shibboleth_oauth(request.env["omniauth.auth"], current_user)
 
       if @user
+        data = request.env["omniauth.auth"]
+        # @user.first_name = data["info"]["name"]
+        @user.last_name = data["info"]["last_name"]
+        @user.person_number = data["info"]["person_number"]
+        @user.save!
         sign_in_and_redirect @user, event: :authentication # this will throw if @user is not activated
         set_flash_message(:notice, :success, kind: "Shibboleth") if is_navigational_format?
       else
         # Skip sign up for CMU Shibboleth user
         data = request.env["omniauth.auth"]
-        @user = User.where(email: data["uid"]).first # email is uid in our case
+        # @user = User.where(email: data["uid"]).first # email is uid in our case (CMU)
+        @user = User.where(email: data["uid"]+"@buffalo.edu").first # email is uid in our case (UB)
 
         # If user doesn't exist, create one first
         if @user.nil?
           @user = User.new
-          @user.email = data["uid"]
+          # @user.email = data["uid"]
+          @user.email = data["uid"]+"@buffalo.edu"
 
           # Set user info based on LDAP lookup
+          if @user.email.include? "@buffalo.edu"
+            @user.first_name = data["info"]["name"]
+            @user.last_name = data["info"]["last_name"]
+            @user.person_number = data["info"]["person_number"]
+          end
+
           if @user.email.include? "@andrew.cmu.edu"
             ldapResult = User.ldap_lookup(@user.email.split("@")[0])
             if ldapResult
