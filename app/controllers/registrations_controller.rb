44e7a3db227398713a688d10f706b86d0f01f9c7
Nicholas Myers <32116122+NicholasMy@users.noreply.github.com>
2023-05-30 16:02:49 -0400

    Disable registration, restyle login screen, change some wording, fix the 404 page image

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,10 +1,15 @@
-class RegistrationsController < Devise::RegistrationsController
-protected
+# class RegistrationsController < Devise::RegistrationsController
+# protected
+#
+#   # After hitting the 'Sign up' page, redirect users here so that they see the
+#   # confirmation flash message ("check your email...")
+#   def after_inactive_sign_up_path_for(_resource)
+#     # /auth/users/sign_in
+#     new_user_session_path
+#   end
+# end
 
-  # After hitting the 'Sign up' page, redirect users here so that they see the
-  # confirmation flash message ("check your email...")
-  def after_inactive_sign_up_path_for(_resource)
-    # /auth/users/sign_in
-    new_user_session_path
-  end
+class RegistrationsController < ApplicationController
+  # This empty controller prevents registration without breaking profile updates
 end
+
