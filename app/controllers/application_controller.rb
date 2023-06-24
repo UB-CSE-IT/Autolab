4275d4367edb840948113bf79d36aed98f989ec1
Damian Ho <damian_ho_xu_yang@yahoo.com>
2023-06-24 12:52:52 -0400

    Use custom error pages (#1921)
    * Use custom error pages
    
    * Revert config.consider_all_requests_local
    
    * Update tests
    
    * Render instead of redirect
    
    * Remove unnecessary error routes
    
    * Delete 404 and 500 files
    
    (cherry picked from commit b8831e319d11c3636342d191c90567f1782b257d)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -21,10 +21,6 @@ class ApplicationController < ActionController::Base
   before_action :set_breadcrumbs
   after_action :log_request_user
 
-  rescue_from ActionView::MissingTemplate do |_exception|
-    redirect_to("/home/error_404")
-  end
-
   # this is where Error Handling is configured. this routes exceptions to
   # the error handler in the HomeController, unless we're in development mode
   #
@@ -160,14 +156,14 @@ protected
                   (params[:controller] == "courses" ? params[:name] : nil)
     @course = Course.find_by(name: course_name) if course_name
 
-    render file: Rails.root.join("public/404.html"), status: :not_found and return unless @course
+    render("home/error_404") && return unless @course
 
     # set course logger
     begin
       COURSE_LOGGER.setCourse(@course)
     rescue StandardError => e
       flash[:error] = e.to_s
-      redirect_to(controller: :home, action: :error) && return
+      render("home/error_500") && return
     end
     ASSESSMENT_LOGGER.setCourse(@course)
   end
@@ -395,7 +391,7 @@ private
           end
         end
 
-        render "home/error"
+        render "home/error_500"
       end
       format.json { head :internal_server_error }
       format.js { head :internal_server_error }
