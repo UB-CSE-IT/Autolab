33cb761ae9cf12b85f4b3357d37e1b2561a0acc2
Nicholas Myers <32116122+NicholasMy@users.noreply.github.com>
2024-02-08 09:38:34 -0500

    Add include_instructors boolean to scoreboard schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_01_01_084757) do
+ActiveRecord::Schema.define(version: 2024_02_08_143255) do
 
   create_table "active_storage_attachments", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name", null: false
@@ -333,6 +333,7 @@ ActiveRecord::Schema.define(version: 2024_01_01_084757) do
     t.integer "assessment_id"
     t.text "banner"
     t.text "colspec"
+    t.boolean "include_instructors", default: false
   end
 
   create_table "scores", id: :integer, charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
