2f16a71af790d8666bf38b71a3f94f7da4027928
Nicholas Myers <32116122+NicholasMy@users.noreply.github.com>
2025-01-10 14:17:44 -0500

    Remove google_oauth2

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable,
          :confirmable
 
-  devise :omniauthable, omniauth_providers: [:shibboleth, :google_oauth2]
+  devise :omniauthable, omniauth_providers: [:shibboleth]
 
   has_many :course_user_data, dependent: :destroy
   has_many :courses, through: :course_user_data
